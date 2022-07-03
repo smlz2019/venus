37ed4c4519944657edf6cd69c17fc312367aa049
smlz2019 <2842825082@qq.com>
2022-07-03 21:32:14 +0800

    vs-common:通用配置模块初始化

---
+++ vs-service/vs-common/src/main/java/com/vs/ad/conf/WebConfiguration.java
@@ -0,0 +1,27 @@
+package com.vs.ad.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * 通用配置定义
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 自定义消息转换器；
+     * springBoot中的其他消息转换器不用了，故清空List;
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+}
